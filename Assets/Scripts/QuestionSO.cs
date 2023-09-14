5cf044398dbe5fdfdf5ea0d20e029cc5615b0ea5
ilya-nuhi <nuhi18@itu.edu.tr>
2023-09-15 02:20:05 +0300

    TextMeshProUGUI

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,8 +6,18 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject {
     [TextAreaAttribute(4,5)]
     [SerializeField] string question = "Enter new question text here.";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion(){
         return question;
+    }
+
+    public int GetCorrectAnswerIndex(){
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index){
+        return answers[index];
     }
 }
